06c6b1f5c367d79dffe2ba12c84f1b05ef98f9c5
roman <roman.moreno226@live.com>
2024-02-15 17:52:35 -0600

    added tailwind to project and updated UI

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -12,15 +12,19 @@ const Dashboard = () => {
     }
 
     return (
-        <div>
-            <h1>Dashboard</h1>
-            <div>
+        <div className="flex items-center justify-center">
+             <div className="">
+                <h1>Dashboard</h1>
                 <p>Logged in as {pb!.authStore!.model!.username}</p>
                 <div className="chats">
                     <ChatBox />
                 </div>
                 <br />
-                <button onClick={handleLogout}>Logout</button>
+                <button
+                    className="flex-shrink-0 bg-red-500 hover:bg-red-700 border-red-500 hover:border-red-700 text-sm border-4 text-white py-1 px-2 rounded-full"
+                    onClick={handleLogout}>
+                    Logout
+                </button>
             </div>
         </div> 
     );
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react()
+  ],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss,
+      ]
+    },
+  },
   server: {
     port: 3000,
   },
